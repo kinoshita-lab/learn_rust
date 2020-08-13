947922b0d5528b7ff8a143109b2a721c85439304
Kazuki Saita <saita@kinoshita-lab.org>
2020-08-13 12:10:13 +0900

    introduce multiple files

---
+++ src/sort.rs
@@ -0,0 +1,18 @@
+
+use rand::prelude::*;
+pub fn generate_random_array(len: usize) -> Vec<u32> {
+    let mut rng = rand::thread_rng();
+    let mut nums: Vec<u32> = (0..len as u32).collect();
+    nums.shuffle(&mut rng);
+    nums
+}
+
+pub fn bubblesort(nums: &mut Vec<u32>) {
+    for i in 0..nums.len() - 1 {
+        for j in 1..nums.len() - i {
+            if nums[j] < nums[j - 1] {
+                nums.swap(j, j - 1)
+            }
+        }
+    }
+}
