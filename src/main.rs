aedaad5b72471b0e11da280fc22dbaa410cb95d5
Kazuki Saita <saita@kinoshita-lab.org>
2020-08-13 12:07:19 +0900

    introduce module

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
-use rand::prelude::*;
+mod sort {
+    use rand::prelude::*;
+    pub fn generate_random_array(len: usize) -> Vec<u32> {
+        let mut rng = rand::thread_rng();
+        let mut nums: Vec<u32> = (0..len as u32).collect();
+        nums.shuffle(&mut rng);
+        nums
+    }
 
-fn generate_random_array(len: usize) -> Vec<u32> {
-    let mut rng = rand::thread_rng();
-    let mut nums: Vec<u32> = (0..len as u32).collect();
-    nums.shuffle(&mut rng);
-    nums
-}
-
-fn bubblesort(nums: &mut Vec<u32>) {
-    for i in 0..nums.len() - 1 {
-        for j in 1..nums.len() - i {
-            if nums[j] < nums[j - 1] {
-                nums.swap(j, j - 1)
+    pub fn bubblesort(nums: &mut Vec<u32>) {
+        for i in 0..nums.len() - 1 {
+            for j in 1..nums.len() - i {
+                if nums[j] < nums[j - 1] {
+                    nums.swap(j, j - 1)
+                }
             }
         }
     }
 }
 
 fn main() {
-    let mut nums = generate_random_array(100);
+    let mut nums = sort::generate_random_array(100);
     println!("unsorted: {:?}", nums);
-    bubblesort(&mut nums);
+    sort::bubblesort(&mut nums);
     println!("sorted: {:?}", nums);
 }
